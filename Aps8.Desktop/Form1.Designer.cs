de9a228b680b5e9c53cb5b96ceabdcd3fb5c4301
Gilberto Junior <contato.gil.tulo@gmail.com>
2023-11-16 01:33:58 -0300

    Adicionar arquivos de projeto.

---
+++ Aps8.Desktop/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Aps8.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnNovaCidade = new Button();
+            lsvCidades = new ListView();
+            Id = new ColumnHeader();
+            Nome = new ColumnHeader();
+            lblNome = new Label();
+            txbNome = new TextBox();
+            btnExcluirCidade = new Button();
+            SuspendLayout();
+            // 
+            // btnNovaCidade
+            // 
+            btnNovaCidade.Location = new Point(12, 51);
+            btnNovaCidade.Name = "btnNovaCidade";
+            btnNovaCidade.Size = new Size(84, 23);
+            btnNovaCidade.TabIndex = 0;
+            btnNovaCidade.Text = "Nova Cidade";
+            btnNovaCidade.UseVisualStyleBackColor = true;
+            btnNovaCidade.Click += btnNovaCidade_Click;
+            // 
+            // lsvCidades
+            // 
+            lsvCidades.Columns.AddRange(new ColumnHeader[] { Id, Nome });
+            lsvCidades.Location = new Point(12, 108);
+            lsvCidades.Name = "lsvCidades";
+            lsvCidades.Size = new Size(704, 330);
+            lsvCidades.TabIndex = 3;
+            lsvCidades.UseCompatibleStateImageBehavior = false;
+            lsvCidades.View = View.Details;
+            // 
+            // Id
+            // 
+            Id.Text = "Id";
+            Id.Width = 350;
+            // 
+            // Nome
+            // 
+            Nome.Text = "Nome";
+            Nome.Width = 350;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(14, 13);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 4;
+            lblNome.Text = "Nome";
+            // 
+            // txbNome
+            // 
+            txbNome.Location = new Point(60, 10);
+            txbNome.Name = "txbNome";
+            txbNome.Size = new Size(135, 23);
+            txbNome.TabIndex = 5;
+            // 
+            // btnExcluirCidade
+            // 
+            btnExcluirCidade.Location = new Point(102, 51);
+            btnExcluirCidade.Name = "btnExcluirCidade";
+            btnExcluirCidade.Size = new Size(93, 23);
+            btnExcluirCidade.TabIndex = 6;
+            btnExcluirCidade.Text = "Excluir Cidade";
+            btnExcluirCidade.UseVisualStyleBackColor = true;
+            btnExcluirCidade.Click += btnExcluirCidade_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(736, 450);
+            Controls.Add(btnExcluirCidade);
+            Controls.Add(txbNome);
+            Controls.Add(lblNome);
+            Controls.Add(lsvCidades);
+            Controls.Add(btnNovaCidade);
+            Name = "Form1";
+            Text = "QualyAir";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNovaCidade;
+        private ListView lsvCidades;
+        private ColumnHeader Id;
+        private ColumnHeader Nome;
+        private Label lblNome;
+        private TextBox txbNome;
+        private Button btnExcluirCidade;
+    }
+}
---
+++ Aps8.Desktop/Form1.cs
@@ -0,0 +1,112 @@
+using Aps8.Desktop.Models;
+using Newtonsoft.Json;
+
+namespace Aps8.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public string url = "https://localhost:40000/api/cidades";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarDados();
+        }
+
+        private async void btnNovaCidade_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txbNome.Text))
+            {
+                return;
+            }
+
+            await AddCidade(txbNome.Text);
+
+            CarregarDados();
+        }
+
+        private async void btnExcluirCidade_Click(object sender, EventArgs e)
+        {
+            if (lsvCidades.Items.Count > 0)
+            {
+                ListViewItem itemSelecionado = lsvCidades.SelectedItems[0];
+
+                await DelCidade(itemSelecionado.Text);
+
+                CarregarDados();
+            }
+        }
+
+        #region Métodos Privados/Auxiliares
+        public async void CarregarDados()
+        {
+            lsvCidades.Items.Clear();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    List<Cidade> cidades = JsonConvert.DeserializeObject<List<Cidade>>(responseBody);
+
+                    foreach (var cidade in cidades)
+                    {
+                        ListViewItem item = new ListViewItem(cidade.Id.ToString());
+                        item.SubItems.Add(cidade.Nome);
+
+                        lsvCidades.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Falha na solicitação GET. Código de status: {response.StatusCode}");
+                }
+            }
+        }
+
+        public async Task AddCidade(string cidade)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var urlFull = $"{url}?nome={cidade}";
+
+                HttpResponseMessage response = await client.PostAsync(urlFull, null);
+
+                // Verifica se a solicitação foi bem-sucedida
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Solicitação POST bem-sucedida. Resposta do servidor: " + responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Falha na solicitação POST. Código de status: {response.StatusCode}");
+                    // Adiciona mais informações para ajudar a diagnosticar o problema, se necessário
+                    Console.WriteLine("Conteúdo da resposta: " + await response.Content.ReadAsStringAsync());
+                }
+            }
+        }
+
+        private async Task DelCidade(string id)
+        {
+            var urlFull = $"{url}/{id}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.DeleteAsync(urlFull);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Solicitação DELETE bem-sucedida!");
+                }
+                else
+                {
+                    Console.WriteLine($"Falha na solicitação DELETE. Código de status: {response.StatusCode}");
+                }
+            }
+        }
+        #endregion
+    }
+}
